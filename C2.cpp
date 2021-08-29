7262085c80759f2f446028990a12eacfcc48685e
sanchaysn <sanchay.sn@gmail.com>
2021-08-29 11:51:38 +0530

    This file contains programs with comments.

---
+++ C2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "No comments" << endl; // This is a one line comment.
+    cout << "Comments" << endl; /* This is a comment which contains more than one line */
+    return 0;
+}
