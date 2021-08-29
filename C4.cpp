934d86c9e9553de75550c3a3e0e7c7be2db374cb
sanchaysn <sanchay.sn@gmail.com>
2021-08-29 23:17:58 +0530

    Basic Calculator.

---
+++ C4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int x;
+  int y;
+  cout << "Enter the value of X = " << endl;
+  cin >> x ;
+  cout << "Enter the value of Y = " << endl;
+  cin >> y ;
+  int z = x+y;
+  cout << "Sum : " << z << endl;
+  return 0;
+}
