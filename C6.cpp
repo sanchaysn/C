b84ab9769e91c303e1a7fcd31c22ef711436d844
sanchaysn <sanchay.sn@gmail.com>
2021-08-30 14:16:38 +0530

    Math is fun.

---
+++ C6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+  std::cout << "In this I will be doing maths and i will also not use \"using namespace\" std to define namespace\n" << std::endl;
+  std::cout << "Maximum value in 5 and 10 is : " << std::max(5, 10) << std::endl;
+  std::cout << "Minimum value in 5 and 10 is : " << std::min(5, 10) << std::endl;
+  return 0;
+}
